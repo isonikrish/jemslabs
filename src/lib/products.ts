f30f26fd32aeea9fe64851d2c22f5dd8c591106a
Krish Soni <isonikrish@gmail.com>
2025-04-07 20:54:01 +0530

    added_popply_in_produtcs

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,8 +1,14 @@
 import hyrly from "@/assets/hyrly.png";
+import popply from "@/assets/popply.png";
 export const products = [
   {
     description: "Hyrly - Task based hiring platform",
     image: hyrly,
     liveLink: "https://hyrly.netlify.app",
+  },
+  {
+    description: "Popply - Create shops in minutes. Launch for a week.",
+    image: popply,
+    liveLink: "https://popply.netlify.app",
   },
 ];
