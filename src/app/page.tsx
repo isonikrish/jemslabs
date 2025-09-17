0aafaff7eff41941623a11e598179168777d2672
Krish Soni <isonikrish@gmail.com>
2025-09-17 22:10:04 +0530

    fix: fixed deployment errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import FAQSection from "@/components/FAQ";
 import Hero from "@/components/Hero";
+import LeadTrackerWrapper from "@/components/LeadTrackerWrapper";
 import OurProcess from "@/components/OurProcess";
 import OurProducts from "@/components/OurProducts";
 import Services from "@/components/Services";
-import LeadTracker from "@/lib/leadTracker";
 
 
 function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-black text-white text-center w-full">
-      <LeadTracker />
+      <LeadTrackerWrapper />
       <Hero />
       <OurProducts />
       <Services />
---
+++ src/components/LeadTrackerWrapper.tsx
@@ -0,0 +1,6 @@
+"use client";
+import LeadTracker from "@/lib/leadTracker";
+
+export default function LeadTrackerWrapper() {
+  return <LeadTracker />;
+}
